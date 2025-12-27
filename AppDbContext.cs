cc7c656834361cf6087614f5a8495f4dad0ec859
Alaa Abu Mussa <166786217+alaaabumusa@users.noreply.github.com>
2025-12-27 17:05:14 +0200

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFOne
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(
+                @"Server=.;Database=EFUserDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+        }
+    }
+}
